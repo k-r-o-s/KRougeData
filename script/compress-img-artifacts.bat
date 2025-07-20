1a02c3c121dd37956c0cea37ebded1f1b622942a
kros <kros_163_kros@163.com>
2025-07-20 23:42:19 +0800

    add compressed images, which are 1/10 of the size of orginal files

---
+++ script/compress-img-artifacts.bat
@@ -0,0 +1,21 @@
+@echo off
+setlocal enabledelayedexpansion
+
+call :CompressPictures "..\img\artifacts" "..\image\artifacts"
+goto :eof
+
+:CompressPictures
+
+:: 确保输出文件夹存在
+if not exist "%~2" (mkdir -p "%~2")
+:: 循环处理所有PNG文件
+for %%f in ("%~1\*.png") do (
+    echo Processing "%%f"...
+    set "filename=%~2\%%~nf"
+    :: 执行FFmpeg命令
+    ffmpeg -i "%%f" -vf scale=96:-1 -quality 75 !filename!.webp
+    if !ERRORLEVEL! neq 0 (
+        exit /b 1
+    )
+)
+goto :eof
